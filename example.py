bb56d07ecc132b28a3d3ee05cf06ab511a73f084
jam1garner <jam1.mcleod@hotmail.com>
2017-06-22 14:12:43 -0400

    Add string support to example

--- example.py
+++ example.py
@@ -1,12 +1,25 @@
 import sys
 from msc import *
 
-with open(sys.argv[1], 'rb') as f:
-    mscFile = MscFile()
+fname = sys.argv[1]
+
+mscFile = MscFile()
+
+with open(fname, 'rb') as f:
     mscFile.readFromFile(f)
-    with open('dump.txt', 'w') as out:
-        for script in mscFile:
-            print((' ' * 20) + script.name, file=out)
-            print('-' * 50, file=out)
-            for command in script:
-                print(command, file=out)
+
+mscFile.addDebugStrings()
+
+with open('dump.txt', 'w') as out:
+    scriptNum = 0
+    for script in mscFile:
+        print((' ' * 20) + script.name, file=out)
+        print('-' * 50, file=out)
+        commandNum = 0
+        for command in script:
+            print(command, file=out)
+            commandNum += 1
+        scriptNum += 1
+    print('',file=out)
+    for string in mscFile.strings:
+        print('.string      "'+string+'"',file=out)
