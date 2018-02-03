1dfaece0e1c24fc9fb2a4353154366d828b9b653
jam1garner <jam1.mcleod@hotmail.com>
2018-02-03 12:42:28 -0500

    Add batch file forwarders back

---
+++ mscHelp.bat
@@ -0,0 +1,6 @@
+@echo off
+echo asm - assemble an MSC File
+echo disasm - disassemble an MSC File
+echo printScript - print a script from an MSC File
+echo scriptOffsets - print offsets from an MSC File
+echo mscHelp - print all the available MSC Commands
---
+++ printScript.bat
@@ -0,0 +1,2 @@
+@echo off
+python %~dp0\printMscScript.py %1 %2
